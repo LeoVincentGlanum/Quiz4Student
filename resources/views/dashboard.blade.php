4a6c20f3850a2ebfe4be4e9d51ff841b81d5a49e
btssi <btssio.quillianchardon@gmail.com>
2023-04-28 13:03:21 +0200

    feat : rajout stats

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,10 @@
     <div class="container">
         <div class="row justify-content-center p-1" role="group" aria-label="Basic example">
             <a href="{{route('concept.index')}}" class="btn btn-outline-primary col-4 qs-little-m-1 ">Concept</a>
-            <div  class="btn btn-outline-primary col-3 active qs-little-m-1"><i
+            <div class="btn btn-outline-primary col-3 active qs-little-m-1"><i
                     class="fa-solid fa-house text-light"></i></div>
-            <a href="{{route('concept.index')}}" type="button" class="btn btn-outline-info col-4 qs-little-m-1">Cours</a>
+            <a href="{{route('concept.index')}}" type="button"
+               class="btn btn-outline-info col-4 qs-little-m-1">Cours</a>
         </div>
 
         {{--Concept--}}
@@ -111,10 +112,21 @@
 
         {{--BTN--}}
 
-        <div class="d-flex justify-content-between mt-3">
+        <div class="d-flex justify-content-between mt-3 mb-2">
             <div></div>
             {{--            <button type="button" class="btn qs-btn-reviser">Tout réviser</button>--}}
             <button type="button" class="btn btn-info">Réviser</button>
+        </div>
+
+        {{--SCORE--}}
+        <div class="row justify-content-center p-1" role="group" aria-label="Basic example">
+            <span class="badge bg-danger col-4 qs-no-width ">Initiation <span class="badge qs-bg-grey ">4</span></span>
+            <span class="badge bg-primary col-4 qs-no-width ">Compréhension <span class="badge qs-bg-grey ">4</span></span>
+            <span class="badge bg-success col-4 qs-no-width ">Maîtrise <span class="badge qs-bg-grey ">4</span></span>
+        </div>
+        <div class="row justify-content-center p-1" role="group" aria-label="Basic example">
+            <span class="badge bg-info col-12 w-100 ">Oubli <span class="badge qs-bg-grey ">4</span></span>
+
         </div>
     </div>
 
